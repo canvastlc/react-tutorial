17f84a3bbf2e5737eb924e306dad1385b4de4fd9
user <user@users-MacBook-Pro.local>
2020-04-29 01:13:14 +0800

    Updates

---
+++ precache-manifest.be341e86c60970d9e351f82fb31c73a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "271c4f0d87bad568bcb82377a71836f5",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "95933863dc38f46b896d",
+    "url": "/react-tutorial/static/css/main.bfc65a53.chunk.css"
+  },
+  {
+    "revision": "607c18d14458d18821eb",
+    "url": "/react-tutorial/static/js/2.f6ae0eff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tutorial/static/js/2.f6ae0eff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95933863dc38f46b896d",
+    "url": "/react-tutorial/static/js/main.c75d68f3.chunk.js"
+  },
+  {
+    "revision": "32d96f51efef0a2c7187",
+    "url": "/react-tutorial/static/js/runtime-main.079d628d.js"
+  }
+]);
